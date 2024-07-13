afe2727ef816c75ed402909fa04486149387c90a
Jhoan5 <sebastiandiaz4757@hotmail.com>
2024-07-12 20:55:10 -0500

    ✨ Más cosas sobre TypeScript

--- projects/basic/index.js
+++ projects/basic/index.js
@@ -51,4 +51,95 @@ function greet(name) {
     console.log("Hello, " + name);
 }
 greet.property = "hello";
-//doSomething(greet)
+class helloClass {
+    constructor(name) {
+        if (name) {
+            console.log("hello " + name);
+        }
+        else {
+            console.log("hello my friend");
+        }
+    }
+}
+function doSomething2(fn) {
+    new fn("Carlos");
+}
+//doSomething2(helloClass)
+// Generics and type parameters
+function firstElement(arr) {
+    return arr[0];
+}
+const s = firstElement(["a", "b", "c"]); // "a"
+const n = firstElement([1, 2, 3]); // 1
+const u = firstElement([]); // undefined
+function map(arr, func) {
+    return arr.map(func);
+}
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
+// En este caso el tipo es un objeto con una propiedad 'length'
+// Entonces los valores que se le pasen a 'a' y 'b' deben tener esa propiedad
+function longest(a, b) {
+    if (a.length >= b.length) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+const longerArray = longest([1, 2], [1, 2, 3]);
+const longerString = longest("Alice", "bob");
+// Error! Numbers don't have a 'length' property
+//const notOK = longest(10, 100);
+function minimumLength(obj, minimum) {
+    if (obj.length >= minimum) {
+        return obj;
+    }
+    else {
+        // Error! 'minimum' and 'obj' have incompatible types
+        //return { length: minimum };
+        return Object.assign(Object.assign({}, obj), { length: minimum });
+    }
+}
+// ✅ usar siempre la menor cantidad de type parameter
+function filter1(arr, func) {
+    return arr.filter(func);
+}
+// ❌
+function filter2(arr, func) {
+    return arr.filter(func);
+}
+function len(x) {
+    return x.length;
+}
+const e1 = len("hello");
+const e2 = len([1, 2, 3]);
+// Parameter Destructuring
+// ❌
+function whatever({ a, b, c }) {
+    const d = a + b + c;
+}
+function whatever2({ a, b, c }) {
+    const d = a + b + c;
+}
+function fn77() {
+    // @ts-expect-error
+    return 1;
+}
+;
+;
+let newPerson = {
+    name: "John",
+    age: 30
+};
+let readNewPerson = newPerson;
+//console.log(readNewPerson.name + " is " + readNewPerson.age + " years old.");
+// Index signature
+let diccionario;
+diccionario = {
+    "uno": [1],
+    "dos": [2],
+    "tres": [3]
+};
+function add77(a) {
+    return a.a + a.b;
+}
--- projects/basic/index.ts
+++ projects/basic/index.ts
@@ -74,3 +74,146 @@ function greet(name: string) {
 }
 greet.property = "hello";
 //doSomething(greet)
+
+// Call signature with new
+type SomeConstructor= {
+  new (s?: string): void
+}
+class helloClass{
+  constructor(name?:string){
+    if(name){
+      console.log("hello " + name);
+    }else{
+      console.log("hello my friend");
+    }
+  }
+}
+function doSomething2(fn: SomeConstructor){
+  new fn("Carlos");
+}
+//doSomething2(helloClass)
+
+// Generics and type parameters
+function firstElement<Type>(arr: Type[]): Type | undefined {
+  return arr[0];
+}
+const s = firstElement(["a", "b", "c"]);// "a"
+const n = firstElement([1, 2, 3]);// 1
+const u = firstElement([]);// undefined
+
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+  return arr.map(func);
+}
+
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
+
+// En este caso el tipo es un objeto con una propiedad 'length'
+// Entonces los valores que se le pasen a 'a' y 'b' deben tener esa propiedad
+function longest<Type extends { length: number }>(a: Type, b: Type) {
+  if (a.length >= b.length) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+const longerArray = longest([1, 2], [1, 2, 3]);
+const longerString = longest("Alice", "bob");
+// Error! Numbers don't have a 'length' property
+//const notOK = longest(10, 100);
+
+function minimumLength<Type extends { length: number }>(
+  obj: Type,
+  minimum: number
+): Type {
+  if (obj.length >= minimum) {
+    return obj;
+  } else {
+    // Error! 'minimum' and 'obj' have incompatible types
+    //return { length: minimum };
+    return {...obj, length: minimum}
+  }
+}
+// ✅ usar siempre la menor cantidad de type parameter
+function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
+  return arr.filter(func);
+}
+// ❌
+function filter2<Type, Func extends (arg: Type) => boolean>(
+  arr: Type[],
+  func: Func
+): Type[] {
+  return arr.filter(func);
+}
+
+// overloads
+// Si es posible usar union types en los parámetros usarlos en vez de overloads
+function len(s: string): number;
+function len(arr: any[]): number;
+function len(x: any) {
+  return x.length;
+}
+const e1 = len("hello");
+const e2 = len([1, 2, 3]);
+
+// Parameter Destructuring
+// ❌
+function whatever({a,b,c}:{a:number, b:number, c:number}):void{
+  const d = a+b+c
+}
+// ✅
+type ABC = {a:number, b:number, c:number}
+function whatever2({a, b, c}:ABC):void{
+  const d = a+b+c
+}
+
+type voidFunc = () => void
+function fn77():void{
+  // @ts-expect-error
+  return 1
+}
+
+// Readonly
+interface Person {
+  name: string;
+  age: number;
+};
+// gracias a readonly no es posible modificar las propiedades name y age
+interface ReadPerson {
+  readonly name: string,
+  readonly age: number
+};
+let newPerson: Person = {
+  name: "John",
+  age: 30
+}
+let readNewPerson: ReadPerson = newPerson
+
+//console.log(readNewPerson.name + " is " + readNewPerson.age + " years old.");
+
+// Index signature
+let diccionario: {
+  readonly [clave: string]: any[];
+};
+
+diccionario = {
+  "uno": [1],
+  "dos": [2],
+  "tres": [3]
+};
+
+//console.log(diccionario["dos"]); // Imprime 2
+
+// Intersection &
+interface A {
+  a: number;
+}
+interface B {
+  b: number;
+}
+
+type C = A & B;
+
+function add77(a: C) {
+  return a.a + a.b;
+}
